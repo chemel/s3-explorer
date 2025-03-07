931a3e2f0ce6738e4c7986ff4c6831a48cdef15c
Alexis CHEMEL <chemel@users.noreply.github.com>
2025-03-07 18:42:16 +0100

    Option pour list ou pour dump

--- src/Command/BucketListObjectsCommand.php
+++ src/Command/BucketListObjectsCommand.php
@@ -6,6 +6,7 @@ use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use App\Service\S3Service;
 
@@ -24,7 +25,9 @@ class BucketListObjectsCommand extends Command
     protected function configure(): void
     {
         $this
-            ->addArgument('bucket', InputArgument::REQUIRED, 'Bucket name');
+            ->addArgument('bucket', InputArgument::REQUIRED, 'Bucket name')
+            ->addOption('dump', InputOption::VALUE_OPTIONAL, null, 'Dump result')
+        ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -37,7 +40,18 @@ class BucketListObjectsCommand extends Command
             'Bucket' => $bucket
         ]);
 
-        dump($result);
+        if ($input->getOption('dump')) {
+            dump($result);
+            return Command::SUCCESS;
+        }
+
+        if (!isset($result['Contents'])) {
+            return Command::FAILURE;
+        }
+
+        foreach ($result['Contents'] as $item) {
+            $output->writeln($item['Key']);
+        }
 
         return Command::SUCCESS;
     }
